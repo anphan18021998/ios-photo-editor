49cd68d595dc92d27b0403a634b9658ed13db351
Pranav Raj Singh Chauhan <pranavchauhan01@gmail.com>
2018-09-09 19:04:33 +0530

    Update PhotoEditorViewController.swift

--- Photo Editor/Photo Editor/PhotoEditorViewController.swift
+++ Photo Editor/Photo Editor/PhotoEditorViewController.swift
@@ -53,7 +53,7 @@ public final class PhotoEditorViewController: UIViewController {
     var colorsCollectionViewDelegate: ColorsCollectionViewDelegate!
     
     // list of controls to be hidden
-    @objc public var hiddenControls : [ControlBridge] = []
+    @objc public var hiddenControls : [NSString] = []
 
     var stickersVCIsVisible = false
     var drawColor: UIColor = UIColor.black
